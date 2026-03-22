96d30598312b5355289f97bb91bdde4f43ef01a0
TEERAPAT-SUKKASEM <6731503015@lamduan.mfu.ac.th>
2026-03-23 04:47:28 +0700

    Refactor main application entry point to include authentication state management and display LoginScreen for unauthenticated users

--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,19 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-// นำเข้าไฟล์ตั้งค่า Firebase ที่เราใช้คำสั่งสร้างมา
 import 'firebase_options.dart';
+import 'ui/auth/login_screen.dart';
 
 void main() async {
-  // 1. ล็อคให้ Flutter เตรียม Engine ให้พร้อมก่อนรันคำสั่งอื่นๆ
   WidgetsFlutterBinding.ensureInitialized();
-
-  // 2. โหลดไฟล์ .env เพื่อดึงค่า URL และ Key
   await dotenv.load(fileName: ".env");
-
-  // 3. เริ่มต้นการทำงานของ Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // 4. เริ่มต้นการทำงานของ Supabase
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL'] ?? '',
     anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
   );
 
-  // 5. สั่งรันแอปพลิเคชัน
   runApp(const KiangThaiApp());
 }
 
@@ -33,19 +26,32 @@ class KiangThaiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Kiang Thai Service',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      // ตอนนี้เราจะแสดงหน้าจอเปล่าๆ ไว้ก่อน เพื่อทดสอบว่าระบบไม่ Error
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'Kiang Thai Service\nระบบฐานข้อมูลเชื่อมต่อสำเร็จแล้ว!',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
+
+      home: StreamBuilder<AuthState>(
+        stream: Supabase.instance.client.auth.onAuthStateChange,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+
+          final session = snapshot.hasData ? snapshot.data!.session : null;
+
+          if (session != null) {
+            return Scaffold(
+              appBar: AppBar(title: const Text('Kiang Thai Main')),
+              body: const Center(child: Text('ยินดีต้อนรับสู่ระบบจองบริการ!')),
+            );
+          } else {
+            return const LoginScreen();
+          }
+        },
       ),
     );
   }
